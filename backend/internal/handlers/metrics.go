bacaec6925609bc785ec9833954a0d7237295c75
GoladorYeah <dispel063@gmail.com>
2025-11-12 13:19:58 +0100

    fix

--- backend/internal/handlers/metrics.go
+++ backend/internal/handlers/metrics.go
@@ -1,8 +1,9 @@
 package handlers
 
 import (
+	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/adaptor"
+
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
